ba536779a047d9f6a7d72b1384dbc488548b491b
kairat092006-cmd <kairat092006@gmail.com>
2026-04-22 19:48:09 +0500

    Modify app routes and add new components
    Updated routes to replace 'favsongs' with 'tracks' and added 'statistics' and 'player' routes.

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,21 +3,19 @@ import { HomePageComponent } from './pages/home-page.component';
 import { AuthPageComponent } from './pages/auth-page.component';
 import { TracksPageComponent } from './pages/tracks-page.component';
 import { PlaylistsPageComponent } from './pages/playlists-page.component';
-import { PlaylistDetailPageComponent } from './pages/playlist-detail-page.component';
 import { ProfilePageComponent } from './pages/profile-page.component';
-import { SearchPageComponent } from './pages/search-page.component';
-import { SongPageComponent } from './pages/song-page.component';
+import { StatisticsPageComponent } from './pages/statistics-page.component';
 import { RecommendationsPageComponent } from './pages/recommendations-page.component';
+import { PlayerPageComponent } from './pages/player-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent, title: 'Shumaq - Home' },
   { path: 'auth', component: AuthPageComponent, title: 'Shumaq - Auth' },
-  { path: 'favsongs', component: TracksPageComponent, title: 'Shumaq - Favorite Songs' },
+  { path: 'tracks', component: TracksPageComponent, title: 'Shumaq - Tracks' },
   { path: 'playlists', component: PlaylistsPageComponent, title: 'Shumaq - Playlists' },
-  { path: 'playlists/:id', component: PlaylistDetailPageComponent, title: 'Shumaq - Playlist Details' },
-  { path: 'song/:title', component: SongPageComponent, title: 'Shumaq - Song' },
-  { path: 'search', component: SearchPageComponent, title: 'Shumaq - Search' },
   { path: 'profile', component: ProfilePageComponent, title: 'Shumaq - Profile' },
+  { path: 'statistics', component: StatisticsPageComponent, title: 'Shumaq - Statistics' },
   { path: 'recommendations', component: RecommendationsPageComponent, title: 'Shumaq - Recommendations' },
+  { path: 'player', component: PlayerPageComponent, title: 'Shumaq - Player' },
   { path: '**', redirectTo: '' }
 ];
